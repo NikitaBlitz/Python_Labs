9eaa6239c45bd7dda5359a575da99d762774eb5c
NikitaLekomtsev <nikita.lekomtsev2015@yandex.ru>
2023-01-18 18:23:05 +0500

    second option

--- 4.py
+++ 4.py
@@ -10,3 +10,17 @@ print(my_list)
 print(list(my_list_squared))
 
 
+#2 на базе данных полученных при парсинге
+URL = "https://realpython.github.io/fake-jobs/"
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, "html.parser")
+results = soup.find(id="ResultsContainer")
+job_elements = results.find_all("div", class_="card-content")
+jobs= []
+for job in job_elements:
+    title_element = job.find("h2", class_="title")
+    jobs.append(title_element.text.strip())
+
+print(jobs)
+jobs= map(len,jobs)
+print(list(jobs))
